ef7a7f5cc6b665103eb4b4b7df4afc2cf5e52051
Aryan R <risaryan9@gmail.com>
2025-11-30 16:07:18 +0530

    Database schemas SQLAlchamy setup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException
+from . import database
+from . import schema
 
 app = FastAPI()
 
+schema.Base.metadata.create_all(bind=database.engine)
--- app/schema.py
+++ app/schema.py
@@ -1,3 +1,44 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, BigInteger
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+from .database import Base
 
 
+class RawLeads(Base):
+    __tablename__ = "raw_leads"
+
+    id = Column(Integer,primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    ph_number = Column(BigInteger, nullable=False)
+    city = Column(String, nullable=False)
+    state = Column(String, nullable=False)
+    brand = Column(String, nullable=False)
+    budget = Column(Integer, nullable=False)
+    years_exp = Column(Integer, nullable=False)
+    interest = Column(String, nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    
+
+class Leads(Base):
+    __tablename__ = "leads"
+
+    id = Column(Integer,primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False,unique=True)
+    city = Column(String, nullable=False)
+    budget = Column(Integer, nullable=False)
+    brand = Column(String, nullable=False)
+    score = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    
+
+class SalesTeam(Base):
+    __tablename__ = "sales_team"
+    id = Column(Integer,primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False)
